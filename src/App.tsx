065f57609fb29a2dd42331794008eb0b1e357ae6
hlmekki <hamdani.lmekki@gmail.com>
2025-05-03 11:59:58 +0200

    ajout du dossier src

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/main.scss";
+import LoginPage from "./pages/login/LoginPage";
+
+function App() {
+	return (
+		<div>
+			<LoginPage />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,66 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaLinkedin } from "react-icons/fa";
+import "./loginPage.scss";
+
+function LoginPage() {
+	return (
+		<div>
+			<div className="main	" role="main">
+				<hgroup>
+					<h1>Connexion</h1>
+					<p>Accédez à vos Candidatures</p>
+				</hgroup>
+				<form action="" role="form" className="form">
+					<input
+						placeholder="Votre email"
+						type="email"
+						autoComplete="username"
+						name="login"
+						required
+						aria-label="login"
+						aria-required="true"
+						role="login"
+					/>
+					<input
+						placeholder="Votre mot de passe"
+						type="text"
+						required
+						aria-label="password"
+						aria-required="true"
+						autoComplete="current-password"
+						name="password"
+						role="password"
+					/>
+
+					<div className="login-options">
+						<label htmlFor="remember">
+							<input
+								type="checkbox"
+								role="switch"
+								name="remember"
+								id="remember"
+							/>
+							Se souvenir de moi
+						</label>
+
+						<a href="#">Mot de passe oublié ?</a>
+					</div>
+					<input type="submit" value="Se connecter" aria-label="submit" />
+					<hr className="hr-text" aria-hidden="true" role="separator"></hr>
+					<div role="group" className="social-buttons">
+						<button className="button outline">
+							<FcGoogle size={20} aria-hidden="true" />
+							<span>Google</span>
+						</button>
+						<button className="button social-button" style={{}}>
+							<FaLinkedin size={20} color="" aria-hidden="true" />
+							<span>LinkedIn</span>
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default LoginPage;
