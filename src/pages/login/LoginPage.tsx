228c171d4a6f9a215f6bf11a8a19b1e2a01015ab
hlmekki <hamdani.lmekki@gmail.com>
2025-05-04 13:45:34 +0200

    front-end de la page login fini

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,11 +1,13 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaLinkedin } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 import "./loginPage.scss";
 
 function LoginPage() {
 	return (
 		<div>
-			<div className="main	" role="main">
+			<div className="container main " role="main">
 				<hgroup>
 					<h1>Connexion</h1>
 					<p>Accédez à vos Candidatures</p>
@@ -32,23 +34,29 @@ function LoginPage() {
 						role="password"
 					/>
 
-					<div className="login-options">
-						<label htmlFor="remember">
-							<input
-								type="checkbox"
-								role="switch"
-								name="remember"
-								id="remember"
-							/>
-							Se souvenir de moi
-						</label>
-
-						<a href="#">Mot de passe oublié ?</a>
+					<div className="login-option">
+						<div className="div">
+							<a className="primary" href="#">
+								Mot de passe oublié ?
+							</a>
+						</div>
+						<div className="login-options ">
+							<label htmlFor="remember">
+								<input
+									type="checkbox"
+									role="switch"
+									name="remember"
+									id="remember"
+								/>
+								Rester connecté
+							</label>
+						</div>
 					</div>
+
 					<input type="submit" value="Se connecter" aria-label="submit" />
 					<hr className="hr-text" aria-hidden="true" role="separator"></hr>
 					<div role="group" className="social-buttons">
-						<button className="button outline">
+						<button className="button secondary">
 							<FcGoogle size={20} aria-hidden="true" />
 							<span>Google</span>
 						</button>
